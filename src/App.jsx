8bb7210e75fb86ae497bb56f52c8577b035a2020
agolautner <lautner.agoston@gmail.com>
2022-01-12 15:26:48 +0100

    classwork

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,21 @@ function App() {
 
   // let pageName = 'Home';
   let [pageName, setPageName] = useState('Landing');
+  // let inputValue = 'Default Value';
+  let [inputValue, setinputValue] = useState('');
+  const inputShort = inputValue.length < 3;
 
+  const cars = [
+    {brand: "Toyota", model: "yaris", topSpeed: "190km/h"},
+    {brand: "Mercedes", model: "C63", topSpeed: "260km/h"},
+    {brand: "Mazda", model: "6", topSpeed: "210km/h"},
+    {brand: "Mazda", model: "Civic", topSpeed: "200km/h"},
+    {brand: "Audi", model: "A4", topSpeed: "220km/h"},
+  ]
+
+  console.log('render');
+  console.log(inputValue);
+  console.log(inputValue.length);
   return (
     <div className="App">
       <div>
@@ -40,6 +54,24 @@ function App() {
       {pageName === 'Home' && <div>text here</div>}
       {pageName === 'About' && <div><h1>text here</h1></div>}
       {pageName === 'Landing' && <div><input type="text" placeholder='input' /></div>}
+
+      <hr/>
+
+      {/* <input type="text" value={inputValue} /> */}
+      <input type="text" defaultValue={inputValue} onChange={(e) => setinputValue(e.target.value)}/>
+      {/* {inputShort ? <button disabled>SUBMIT</button> : <button>SUBMIT</button>} */}
+      <button disabled={inputShort ? true : false}>SUBMIT</button>
+      {inputShort && <p>min 3 char!</p>}
+
+      <hr />
+
+      {cars.map((c) => <div key={c.brand + c.model}>
+        <p>Brand: {c.brand}</p>
+        <p>Model: {c.model}</p>
+        <p>Top speed: {c.topSpeed}</p>
+      </div>)
+      }
+
     </div>
   );
 }
